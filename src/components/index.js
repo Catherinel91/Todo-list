8cf8e452716db49c8e95babdfd65075c26d94a6a
Kate <199111k@gmail.com>
2021-10-05 16:34:11 +0300

    create todo-list
    (cherry picked from commit 6b90ec2ae6ba9f3b069762bc5bb7dff07dbdf796)

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as AddBtnList} from './AddBtnList/AddBtnList';
+export {default as ColorIcon} from './ColorIcon/ColorIcon';
+export {default as List} from './List/List';
+export {default as Tasks} from './Tasks/Tasks';

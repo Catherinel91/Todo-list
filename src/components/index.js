df537284e36d8d9a198d3e87c69d504288d0b5e7
Алексей Локтев <ARLoktev@sberbank.ru>
2021-10-06 12:13:31 +0300

    Revert "create todo-list"
    This reverts commit 6b90ec2ae6ba9f3b069762bc5bb7dff07dbdf796.

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as AddBtnList} from './AddBtnList/AddBtnList';
-export {default as ColorIcon} from './ColorIcon/ColorIcon';
-export {default as List} from './List/List';
-export {default as Tasks} from './Tasks/Tasks';
